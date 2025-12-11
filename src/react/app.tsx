8298fb44aad85eb1f15af9dcbb67abdcab4797f4
rhy3h <rhy3htw@gmail.com>
2025-12-11 14:25:57 +0800

    chore: support shadcn

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -2,4 +2,4 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<h1 className="text-3xl font-bold underline">Hello world!</h1>);
